6c1d81b0eed75688eca0f2558a47404600171ddd
Matteo Mammoliti <matti.mm04@gmail.com>
2025-11-02 12:30:48 +0100

    Inizializzazione DBNEON + Postgres

--- src/main/java/it/unical/serialmente/UI/SerialmenteMainApp.java
+++ src/main/java/it/unical/serialmente/UI/SerialmenteMainApp.java
@@ -1,4 +1,4 @@
-package it.unical.serialmente.serialmente;
+package it.unical.serialmente.UI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SerialmenteMainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SerialmenteMainApp.class.getResource(""));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
