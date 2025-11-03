69a1e89cf881143181e08f2161e3444de832e282
Matteo Mammoliti <matti.mm04@gmail.com>
2025-11-03 21:53:51 +0100

    Nuove funzioni implementate in TMDbAPI

--- src/main/java/it/unical/serialmente/TechnicalServices/API/TMDbAPI.java
+++ src/main/java/it/unical/serialmente/TechnicalServices/API/TMDbAPI.java
@@ -18,7 +18,45 @@ public class TMDbAPI {
     private final String BASE_URL = "https://api.themoviedb.org/3";
     private final String DEFAULT_LANGUAGE = "it-IT";
     private final String RICHIESTA_DATI = "GET";
-    private Map<String, Integer> generi = new HashMap<>();
+
+    /**
+     * Funzione usata per ottenere film e serietv da consigliare lall'utente nella homepage
+     *
+     * @param generi
+     * @param piattaforme
+     * @return
+     */
+    public String getTitoliConsigliati(List<Integer> generi, List<Integer> piattaforme, String tipologia) throws Exception {
+        return cercaTitoliPerCriteri(tipologia, generi, null, piattaforme);
+    }
+
+
+    /**
+     * Funzione che permette di ottenere il titolo di tipologia {tipologia} ordinati per {tipologiaSort}:
+     * - popolarità = popularity.desc
+     * - ultime uscite = air_date.desc
+     *
+     * @param tipologia
+     * @param tipologiaSort
+     * @return Titolo
+     * @throws Exception
+     */
+    public String getTitoliPiùVisti(String tipologia, String tipologiaSort) throws Exception {
+        String richiesta = "/discover/" + tipologia + "?sort_by=" +  tipologiaSort;
+        return inviaRichiesta(richiesta);
+    }
+
+    /**
+     * Restituisce i dettagli della SerieTV associata ad {idSerie}. L'{idSerie} è quello del DB remoto
+     *
+     * @param idSerie
+     * @return
+     * @throws Exception
+     */
+    public String getSerieTV(Integer idSerie) throws Exception {
+        String richiesta = "/tv/"+ idSerie;
+        return inviaRichiesta(richiesta);
+    }
 
     /**
      * Funzione che consente di ricercare un titolo specifico
@@ -32,7 +70,27 @@ public class TMDbAPI {
      */
     public String cercaTitolo(String nomeTitolo, String tipologia, List<String> generi, Integer annoPubblicazione) throws Exception {
         if(nomeTitolo != null) return cercaTitoloPerNome(nomeTitolo, tipologia);
-        return cercaTitoliPerCriteri(tipologia, getIdGeneri(generi, tipologia), annoPubblicazione);
+        return cercaTitoliPerCriteri(tipologia, getIdGeneri(generi, tipologia), annoPubblicazione, null);
+    }
+
+    /**
+     * Funzione che restituisce le piattaforme streaming dei titoli di tipologia {tipologia}
+     * @param tipologia
+     * @return listaPiattaforme
+     * @throws Exception
+     */
+    public List<String> getPiattaformeDisponibili(String tipologia) throws Exception {
+        String richiesta = "/watch/providers/" +  tipologia;
+        String risposta = inviaRichiesta(richiesta);
+
+        JSONObject json = new JSONObject(risposta);
+        JSONArray jsonArray = json.getJSONArray("results");
+        List<String> lista = new ArrayList<>();
+
+        for(int i = 0; i < jsonArray.length(); i++){
+            lista.add(jsonArray.getJSONObject(i).getString("provider_name"));
+        }
+        return lista;
     }
 
     /**
@@ -91,36 +149,55 @@ public class TMDbAPI {
      * @return String
      * @throws Exception
      */
-    private String cercaTitoliPerCriteri(String tipologia, List<Integer> generi, Integer annoPubblicazione) throws Exception {
+    private String cercaTitoliPerCriteri(String tipologia, List<Integer> generi, Integer annoPubblicazione, List<Integer> piattaforme) throws Exception {
         String richiesta;
 
         if(tipologia != null) richiesta = "/discover/" + tipologia;
         else richiesta = "/discover/multi";
 
-        String richiestaConAggiunteGeneri = richiesta + "?with_genres=";
+        if(generi != null || annoPubblicazione != null || piattaforme != null) richiesta = richiesta + "?";
 
         boolean generiAggiunti = false;
-        if(generi != null) generiAggiunti = true;
+        if(generi != null) {
+            generiAggiunti = true;
+            richiesta = richiesta + "with_genres=";
 
-        for(int i = 0; i < generi.size(); i++) {
-            richiestaConAggiunteGeneri += generi.get(i);
+            for(int i = 0; i < generi.size(); i++) {
+                richiesta += generi.get(i);
 
-            if(i < generi.size()-1) { richiestaConAggiunteGeneri += ","; }
+                if (i < generi.size() - 1) {
+                    richiesta += ",";
+                }
+            }
         }
 
-        if(generiAggiunti) {
-            richiesta = richiestaConAggiunteGeneri;
+        boolean piattaformeAggiunte = false;
+        if(generiAggiunti) { richiesta = richiesta + "&"; }
 
-            if(annoPubblicazione != null) richiesta += "&year=" + annoPubblicazione;
-            else return inviaRichiesta(richiesta);
+        if(piattaforme != null) {
+            piattaformeAggiunte = true;
+            richiesta = richiesta + "with_watch_providers=";
 
-        } else { richiesta = richiesta + "?year=" +  annoPubblicazione; }
+            for(int i = 0; i < piattaforme.size(); i++) {
+                richiesta += piattaforme.get(i);
+
+                if (i < piattaforme.size() - 1) {
+                    richiesta += ",";
+                }
+            }
+        }
+
+        if(piattaformeAggiunte) { richiesta = richiesta + "&"; }
+
+        if(annoPubblicazione != null) {
+            richiesta = richiesta + "year=" +  annoPubblicazione;
+        }
 
         return inviaRichiesta(richiesta);
     }
 
     /**
-     * Funzione di ausilio che invia richieste HTTP GET a TMDb
+     * Funzione di utility che invia richieste HTTP GET a TMDb
      *
      * @param richiesta
      * @return JSON
